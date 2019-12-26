dfb7d2bc4ea26f63f6f49cf223b6f7dab3acb705
paul <pavkascool@gmail.com>
2019-12-26 13:19:24 +0300

    BattleActivity incorrectly processes orientation change

--- app/src/main/java/com/gmail/pavkascool/seabattle/BattleActivity.java
+++ app/src/main/java/com/gmail/pavkascool/seabattle/BattleActivity.java
@@ -1,44 +1,68 @@
 package com.gmail.pavkascool.seabattle;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BattleActivity extends AppCompatActivity {
 
     private CellViewLayout white;
     private CellViewLayout black;
-    private CellView ship;
+    private List<CellView> ships = new ArrayList<CellView>();
     private int cellSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //  super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
-
         white = findViewById(R.id.white);
         black = findViewById(R.id.black);
-        ship = findViewById(R.id.ship);
-        if (savedInstanceState != null) {
-            int location = savedInstanceState.getInt("parent");
-            if(location != R.id.white) {
-                white.removeView(ship);
-                black.addView(ship);
+
+        if(savedInstanceState == null) {
+            Intent intent = getIntent();
+            for(int i = 0; i < 4; i++) {
+                CellView ship = new CellView(this, null);
+                int[] location = intent.getIntArrayExtra("shp" + i);
+                ship.setLocationCol(location[0]);
+                ship.setLocationRow(location[1]);
+                ship.setCols(location[2]);
+                ship.setRows(location[3]);
+                ships.add(ship);
+                white.addView(ship);
             }
-            ship.setLocationCol(savedInstanceState.getInt("locCol"));
-            ship.setLocationRow(savedInstanceState.getInt("locRow"));
         }
+        else {
+            for(int i = 0; i < ships.size(); i++) {
+                CellView ship = new CellView(this, null);
+                int[] location = savedInstanceState.getIntArray("ship" + i);
+                ship.setLocationCol(location[0]);
+                ship.setLocationRow(location[1]);
+                ship.setCols(location[2]);
+                ship.setRows(location[3]);
+                ships.add(ship);
+                white.addView(ship);
 
-        super.onCreate(savedInstanceState);
-
+            }
+        }
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putInt("parent", ((ViewGroup)ship.getParent()).getId());
-        savedInstanceState.putInt("locCol", ship.getLocationCol());
-        savedInstanceState.putInt("locRow", ship.getLocationRow());
+        for(int i = 0; i < ships.size(); i++) {
+            CellView ship = ships.get(i);
+            int[] loc = new int[4];
+            loc[0] = ship.getLocationCol();
+            loc[1] = ship.getLocationRow();
+            loc[2] = ship.getCols();
+            loc[3] = ship.getRows();
+            savedInstanceState.putIntArray("ship" + i, loc);
+        }
+
     }
 }
